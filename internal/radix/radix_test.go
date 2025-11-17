de4de988b4625e5f7efa89a8828d3e0348465fdd
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-11-17 09:55:02 -0600

    cleans up test with a couple util functions

--- internal/radix/radix_test.go
+++ internal/radix/radix_test.go
@@ -8,29 +8,38 @@ import (
 	"github.com/elmq0022/krillin/router"
 )
 
+func MakeTestHandler(value any) router.Handler {
+	return func(req *http.Request) (int, any, error) {
+		return 0, value, nil
+	}
+}
+
+func ReadTestHandler(h router.Handler) any {
+	fakeReq, _ := http.NewRequest(http.MethodGet, "", nil)
+	_, value, _ := h(fakeReq)
+	return value
+}
+
 func TestNewRadix(t *testing.T) {
 
 	path := "/foo/bar/baz"
 	method := http.MethodGet
-	handler := func(req *http.Request) (int, any, error) { return 200, 1, nil }
+	handler := MakeTestHandler(1)
 
 	routes := router.Routes{
 		{Path: path, Method: method, Handler: handler},
-		{Path: "/foo/bar/baz2", Method: http.MethodPatch, Handler: func(req *http.Request) (int, any, error) { return 200, 2, nil }},
+		{Path: "/foo/bar/baz2", Method: http.MethodPatch, Handler: MakeTestHandler(2)},
 	}
 
 	r, _ := radix.New(routes)
-	fakeReq, _ := http.NewRequest(http.MethodGet, "", nil)
 
 	h, _ := r.Lookup(method, path)
-	_, got, _ := h(fakeReq)
-	if got != 1 {
+	if got := ReadTestHandler(h); got != 1 {
 		t.Fatalf("want %d, got %d", 1, got)
 	}
 
 	h, _ = r.Lookup(http.MethodPatch, "/foo/bar/baz2")
-	_, got2, _ := h(fakeReq)
-	if got2 != 2 {
-		t.Fatalf("want %d, got %d", 2, got2)
+	if got := ReadTestHandler(h); got != 2 {
+		t.Fatalf("want %d, got %d", 2, got)
 	}
 }
