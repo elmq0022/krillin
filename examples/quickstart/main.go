6f3745a8182d01ca11b6e54eddaffd5c7b19c06e
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-11-27 01:14:04 -0600

    fixes bad name

--- examples/quickstart/main.go
+++ examples/quickstart/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/elmq0022/kami/responders.go"
+	"github.com/elmq0022/kami/responders"
 	"github.com/elmq0022/kami/router"
 	"github.com/elmq0022/kami/types"
 )
